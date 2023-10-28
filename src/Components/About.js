2efc5dc09aa0497f4e5ab80c3efbe40c8fe5538d
Srushti Shah <66720879+srush-shah@users.noreply.github.com>
2023-10-28 13:10:33 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -48,7 +48,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a href={resumeDownload} className="button">
+                <a href={resumeDownload} className="button" download="SrushtiShah_Resume.pdf">
                   <i className="fa fa-download"></i>Download Resume
                 </a>
               </p>
